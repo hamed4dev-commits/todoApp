0d21d6d90ec74e61eaebfcc5639024b6495a764e
unknown <hamed4dev@gmail.com>
2026-08-15 14:31:56 +0330

    form updated with zod + react-hook-form

--- components/templates/NewTask.tsx
+++ components/templates/NewTask.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormState, SubmitHandler, useForm } from "react-hook-form";
+import * as z from "zod";
 
 // import { useState } from "react";
 // // import { validation } from "../helpers/helper";
@@ -82,28 +84,71 @@ import { SubmitHandler, useForm } from "react-hook-form";
 // };
 
 // export default NewTask;
-type Inputs = {
-  title: string;
-  description: string;
-};
+type Inputs = z.infer<typeof schema>;
+const schema = z.object({
+  title: z
+    .string()
+    .trim()
+    .min(5, { message: "Title must be at least 5 characters long" })
+    .refine((value) => !/\d/.test(value), {
+      message: "Title must not contain numbers",
+    }),
+  description: z
+    .string()
+    .trim()
+    .min(10, { message: "Description must be at least 10 characters long" })
+    .refine((value) => !/\d/.test(value), {
+      message: "Description must not contain numbers",
+    }),
+});
+
 const NewTask = () => {
-  const {register,handleSubmit,watch,formState:{errors}} = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
-  console.log(watch("title"))
-  console.log(errors)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>({ resolver: zodResolver(schema), mode: "onChange" });
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+  // console.log(watch("title"))
+  // console.log(errors);
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>Title</label>
-          <input {...register("title")} className="border-sky-400 border rounded-2xl  placeholder-gray-400 text-white  bg-gray-700 px-3 py-0.5 " />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-5 mt-10"
+      >
+        <div className="flex flex-col gap-3 w-1/2 h-22">
+          <label htmlFor="title">Title</label>
+          <input
+            id="title"
+            {...register("title")}
+            className="border-sky-400 border rounded-2xl  placeholder-gray-400 text-white  bg-gray-700 px-3 py-0.5 ml-0.5 "
+          />
+          {errors.title && (
+            <p className="text-red-600 -h-5 text-sm">{errors.title.message}</p>
+          )}
         </div>
-        <div>
-          <label>Description</label>
-          <input className="border-sky-400 border rounded-2xl  placeholder-gray-400 text-white  bg-gray-700 px-3 py-0.5 " {...register("description",{required:true})} />
-          {errors.description && <span>enter your description</span>}
+        <div className="flex flex-col gap-3 w-1/2 h-22">
+          <label htmlFor="desc">Description</label>
+          <input
+            id="desc"
+            className="border-sky-400 border rounded-2xl  placeholder-gray-400 text-white  bg-gray-700 px-3 py-0.5 ml-0.5 "
+            {...register("description")}
+          />
+          {errors.description && (
+            <p className="text-red-600 -h-5 text-sm">{errors.description.message}</p>
+          )}
         </div>
-        <button type="submit" >send</button>
+        <button
+          disabled={isSubmitting}
+          type="submit"
+          className="w-fit py-2 px-10 bg-emerald-600 font-semiboldbold mt-3 cursor-pointer rounded-2xl"
+        >
+          {isSubmitting ? "sending..." : "send"}
+        </button>
       </form>
     </div>
   );
